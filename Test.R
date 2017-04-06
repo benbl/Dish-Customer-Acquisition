f1773fa4174adc38e4a8b30af62c2ace65ecf285
benbl <benblanco50@gmail.com>
2017-04-05 23:20:36 -0400

    upload and clean Dish data

--- Test.R
+++ Test.R
@@ -1,1 +1,16 @@
-#Glassdoor API
+#load data
+library(readxl)
+DISH_data_Spring_2017 <- read_excel("C:/Users/Benjamin/Downloads/DISH data Spring 2017.xlsx", 
+                                      +     skip = 2)
+#clean data
+View(DISH_data_Spring_2017)
+data <- DISH_data_Spring_2017
+data <- data[,2:4]
+data <- data[1:84,]
+
+plot(data$`#`,data$`Customers Acquired`)
+
+
+
+
+
